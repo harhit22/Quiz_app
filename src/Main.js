253a34b461ed226b0b9263662c7ae78a5766a6db
Harshit <harshitshrimalee22@gmail.com>
2023-09-12 15:03:34 +0530

    starting quiz

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React, { Children } from 'react';
+
+const Main = () => {
+    return (
+        <main className='main'>
+            {Children}
+        </main>
+    );
+}
+
+export default Main;
