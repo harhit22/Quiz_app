fa60c5d2beba8b8df7a4e1e8cb11163ac318ff90
Harshit <harshitshrimalee22@gmail.com>
2023-09-13 09:27:22 +0530

    quiz finding points issue

--- src/components/App.js
+++ src/components/App.js
@@ -66,6 +66,7 @@ export default function App() {
     initialState
   );
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev, cur)=>prev + cur.points,0)
   useEffect(function () {
     fetch("http://localhost:9000/questions")
       .then((res) => res.json())
@@ -83,7 +84,7 @@ export default function App() {
         )}
         {status === "active" && (
           <>
-            <Progress index={index} numQuestions={numQuestions}/>
+            <Progress index={index} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer}/>
             <Question
               question={questions[index]}
               dispatch={dispatch}
--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Progress = ({index,numQuestions}) => {
-    return (
-        <header className='progress'>
-            <p>Question <strong>{index + 1}</strong>/ {numQuestions}</p>
-            
-        </header>
-    );
+const Progress = ({ index, numQuestions, points, maxPossiblePoints, answer }) => {
+  return (
+    <header className='progress'>
+      <progress max={numQuestions} value={index + Number(answer !==null)} />
+      <p>Question <strong>{index + 1}</strong> / {numQuestions}</p>
+      <p>{points} / {maxPossiblePoints}</p>
+    </header>
+  );
 }
 
 export default Progress;
